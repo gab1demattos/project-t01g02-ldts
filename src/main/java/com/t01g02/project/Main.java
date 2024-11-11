613eaf080960843063b59009d04e1bea93faac9c
Matilde Sousa <up202305502@up.pt>
2024-11-11 17:59:46 +0000

    ..

--- src/main/java/com/t01g02/project/Main.java
+++
@@ -1,9 +0,0 @@
-package com.t01g02.project;
-
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
-    }
-}
