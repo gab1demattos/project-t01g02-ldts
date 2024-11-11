80cf71139e1d4f21c08fac57a9861a5dbdf8e8e8
Matilde Ribeiro <101738882+Matilde-rad@users.noreply.github.com>
2024-11-11 16:46:22 +0000

    Initial version

---
+++ src/main/java/com/t01g02/project/Main.java
@@ -0,0 +1,9 @@
+package com.t01g02.project;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+    }
+}
