915617ed916de61ab2a7619fdb25bb0f8b20392c
Matilde Ribeiro <101738882+Matilde-rad@users.noreply.github.com>
2024-11-25 18:00:24 +0000

    fixed the terminal draw

--- src/main/java/com/t01g02/project/Game.java
+++ src/main/java/com/t01g02/project/Game.java
@@ -1,6 +1,7 @@
 package com.t01g02.project;
 
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.graphics.TextImage;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
@@ -8,10 +9,13 @@ import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFrame;
 import com.t01g02.project.model.CityModel;
 import com.t01g02.project.viewer.CharacterViewer;
 import com.t01g02.project.viewer.CityViewer;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.net.URISyntaxException;
 import java.awt.*;
 import java.io.File;
@@ -27,6 +31,7 @@ public class Game {
     //private final Controller controller;
 
     public Game() throws IOException, FontFormatException, URISyntaxException {
+
         this.city = new CityModel(500, 250); //320/200
 
         URL resource = getClass().getClassLoader().getResource("fonts/square.ttf");
@@ -60,14 +65,25 @@ public class Game {
         city.initializeRoads();
         characterViewer.initializeCharacters();
 
+        ((AWTTerminalFrame)terminal).addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
 
     }
 
     public void run() throws IOException {//this is part of view??
+        cityViewer.initializeCityImage();
 
         while (true) {
+            screen.clear();
+
             cityViewer.draw();
             characterViewer.draw();
+
+            screen.refresh();
 
             KeyStroke key = screen.readInput();
             if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q' || key.getKeyType() == KeyType.EOF) {
--- src/main/java/com/t01g02/project/viewer/CityViewer.java
+++ src/main/java/com/t01g02/project/viewer/CityViewer.java
@@ -2,8 +2,11 @@ package com.t01g02.project.viewer;
 
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.BasicTextImage;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.graphics.TextImage;
 import com.googlecode.lanterna.screen.Screen;
 import com.t01g02.project.model.CityModel;
 import com.t01g02.project.model.Tile;
@@ -13,34 +16,41 @@ import java.io.IOException;
 public class CityViewer {
     private final CityModel city;
     private final Screen screen;
+    private final BasicTextImage cityImage;
     //private GameTimer timer;
 
     public CityViewer(CityModel city, Screen screen) {
         this.city = city;
         this.screen = screen;
+        this.cityImage = new BasicTextImage(500, 250);
 
     }
-
-    public void draw() throws IOException {
-        screen.clear();
-
-        TextGraphics graphics = screen.newTextGraphics();
-        graphics.setBackgroundColor(TextColor.Factory.fromString("#FFE1EA"));
-        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(screen.getTerminalSize().getColumns(), screen.getTerminalSize().getRows()), ' ');
-
+    public void initializeCityImage() {
+        for (int y = 0; y < city.getHeight(); y++) {
+            for (int x = 0; x < city.getWidth(); x++) {
+                // Set a basic tile background color to 'empty' for now
+                TextCharacter defaultChar = new TextCharacter(' ', TextColor.Factory.fromString("#000000"), TextColor.Factory.fromString("#FFE1EA"));
+                cityImage.setCharacterAt(new TerminalPosition(x, y), defaultChar);
+            }
+        }
         for (int y = 0; y < city.getHeight(); y++) {
             for (int x = 0; x < city.getWidth(); x++) {
                 Tile tile = city.getTile(x, y);
                 if (tile != null) {
-                    graphics.setForegroundColor(tile.getColor());
                     char displayChar = tile.getType() == Tile.Type.ROAD ? '█' : ' ';
-                    graphics.putString(new TerminalPosition(x, y), String.valueOf(displayChar));
+                    TextCharacter textChar = new TextCharacter(displayChar, tile.getColor(), TextColor.Factory.fromString("#FFE1EA"));
+                    cityImage.setCharacterAt(new TerminalPosition(x, y), textChar);
                 }
             }
         }
+    }
 
+    public void draw() throws IOException {
+        TextGraphics graphics = screen.newTextGraphics();
+        //graphics.setBackgroundColor(TextColor.Factory.fromString("#FFE1EA"));
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(screen.getTerminalSize().getColumns(), screen.getTerminalSize().getRows()), ' ');
 
-        screen.refresh();
+        graphics.drawImage(new TerminalPosition(0, 0), cityImage);
 
     }
 }
