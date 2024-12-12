74550a5133308f3eb06da96715b30ecdf9893e38
Matilde Duarte <matilde.dar@gmail.com>
2024-12-12 09:48:12 +0000

    ..

--- src/main/java/com/t01g02/project/Game.java
+++ src/main/java/com/t01g02/project/Game.java
@@ -1,31 +1,14 @@
 package com.t01g02.project;
 
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.graphics.TextImage;
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+
 import com.googlecode.lanterna.terminal.swing.AWTTerminalFrame;
 import com.t01g02.project.model.CharacterModel;
 import com.t01g02.project.model.CityModel;
-import com.t01g02.project.model.Element;
 import com.t01g02.project.viewer.CharacterViewer;
 import com.t01g02.project.viewer.CityViewer;
-
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.net.URISyntaxException;
 import java.awt.*;
-import java.io.File;
-import java.net.URL;
 import java.io.IOException;
-import java.util.List;
 
 public class Game {
     private final LanternaGui gui;
