2d4ffb7b17d50e1b03fe2a83135e97de074313d7
gab1mattos <gabydemattos@icloud.com>
2024-12-14 01:31:05 +0000

    fixing popups position

--- src/main/java/com/t01g02/project/model/PopUpsModel.java
+++ src/main/java/com/t01g02/project/model/PopUpsModel.java
@@ -6,6 +6,7 @@ import com.t01g02.project.viewer.Sprite;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -19,7 +20,7 @@ public class PopUpsModel extends Element {
         super(sprite, position, name);
     }
 
-    private static Position randomPosition() {
+/*    private static Position randomPosition() {
         int rx = RANDOM.nextInt(TerminalWidth);
         int ry = RANDOM.nextInt(TerminalHeight);
         return new Position(rx, ry);
@@ -41,30 +42,57 @@ public class PopUpsModel extends Element {
         Tile tile = city.getTile(position.getX(), position.getY());
         return (tile.getType() == Tile.Type.ROAD);
     }
+*/
+
+    private static List<Position> popupsPositions = new ArrayList<>( List.of(
+            new Position(100, 130),
+                new Position(80, 80),
+                new Position(200, 170),
+                new Position(60, 120),
+                new Position(120, 70),
+                new Position(70, 30),
+                new Position(200, 150),
+                new Position(180, 95),
+                new Position(140, 100)
+    ));
+
+    private static void popupsPositions() {
+        while(!popupsPositions.isEmpty()) {
+            getRandomPosition();
+        }
+    }
+
+    public static Position getRandomPosition() {
+        assert !popupsPositions.isEmpty();
+        int randomIndex = RANDOM.nextInt(popupsPositions.size());
+        Position position = popupsPositions.get(randomIndex);
+        popupsPositions.remove(randomIndex);
+        return position;
+    }
 
     public static void initializeSpeedPopUps(Screen screen, CityModel city) throws IOException {
         speedpopups = List.of(
-                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/speed.png"), findValidRandomPosition(city), "Speed"),
-                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/speed.png"), findValidRandomPosition(city), "Speed"),
-                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/speed.png"), findValidRandomPosition(city), "Speed"),
-                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/speed.png"), findValidRandomPosition(city), "Speed")
+                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/speed.png"), getRandomPosition(), "Speed"),
+                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/speed.png"), getRandomPosition(), "Speed"),
+                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/speed.png"), getRandomPosition(), "Speed"),
+                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/speed.png"), getRandomPosition(), "Speed")
 
         );
     }
 
     public static void initializeMudPopUps(Screen screen, CityModel city) throws IOException {
         mudpopups = List.of(
-                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/smallmud.png"), findValidRandomPosition(city), "Mud"),
-                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/smallmud.png"), findValidRandomPosition(city), "Mud"),
-                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/smallmud.png"), findValidRandomPosition(city), "Mud")
+                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/mud.png"), getRandomPosition(), "Mud"),
+                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/mud.png"), getRandomPosition(), "Mud"),
+                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/mud.png"), getRandomPosition(), "Mud")
 
         );
     }
 
     public static void initializeBlockPopUps(Screen screen, CityModel city) throws IOException {
         blockpopups = List.of(
-                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/smallblock.png"), findValidRandomPosition(city), "Block"),
-                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/smallblock.png"), findValidRandomPosition(city), "Block")
+                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/block.png"), getRandomPosition(), "Block"),
+                new PopUpsModel(new Sprite(screen, "src/main/resources/Pop-ups/block.png"), getRandomPosition(), "Block")
         );
     }
 }
