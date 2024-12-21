118997765fd0530d8f8452dd0e31e4799d07b6a2
Matilde Sousa <up202305502@up.pt>
2024-12-21 17:56:43 +0000

    refactoring

--- src/main/java/com/t01g02/project/menu/SettingsView.java
+++ src/main/java/com/t01g02/project/menu/SettingsView.java
@@ -31,36 +31,43 @@ public class SettingsView implements IView{
             screen.clear();
             TextGraphics textGraphics = screen.newTextGraphics();
 
-            textGraphics.setBackgroundColor(new TextColor.RGB(255, 225, 237)); // Pink
-            textGraphics.fillRectangle(new TerminalPosition(0, 0), newSize, ' '); // Fill screen with background color
-
-            int centerX = newSize.getColumns() / 2;
-            int centerY = newSize.getRows() / 2;
-            int boxWidth = 20;
-            int boxHeight = 10;
-
-            // Draw Background Squares
-            drawBox(textGraphics, centerX - boxWidth - 2, centerY - boxHeight / 2, boxWidth, boxHeight);
-            drawBox(textGraphics, centerX + 2, centerY - boxHeight / 2, boxWidth, boxHeight);
-
-            // Exit Info
-            String exitInfo = model.getExitSettingsInfo();
-            textGraphics.setForegroundColor(new TextColor.RGB(217, 167, 164));
-            textGraphics.setBackgroundColor(new TextColor.RGB(255, 225, 237));
-            textGraphics.putString(centerX - exitInfo.length() / 2, centerY - boxHeight / 2 - 2, exitInfo, SGR.BOLD);
-
-            // Enter Info
-            String enterInfo = model.getEnterSettingsInfo();
-            textGraphics.setForegroundColor(new TextColor.RGB(217, 167, 164));
-            textGraphics.setBackgroundColor(new TextColor.RGB(255, 225, 237));
-            textGraphics.putString(centerX - enterInfo.length() / 2, centerY + 9, enterInfo, SGR.BOLD);
-
+            drawBackground(textGraphics, newSize);
+            drawMessages(textGraphics, newSize);
             redrawButtons();
+
             screen.refresh();
 
         }catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void drawBackground(TextGraphics textGraphics, TerminalSize newSize){
+        textGraphics.setBackgroundColor(new TextColor.RGB(255, 225, 237));
+        textGraphics.fillRectangle(new TerminalPosition(0, 0), newSize, ' ');
+    }
+    private void drawMessages(TextGraphics textGraphics, TerminalSize newSize){
+        int centerX = newSize.getColumns() / 2;
+        int centerY = newSize.getRows() / 2;
+        int boxWidth = 20;
+        int boxHeight = 10;
+
+        // Draw Background Squares
+        drawBox(textGraphics, centerX - boxWidth - 2, centerY - boxHeight / 2, boxWidth, boxHeight);
+        drawBox(textGraphics, centerX + 2, centerY - boxHeight / 2, boxWidth, boxHeight);
+
+        // Exit Info
+        String exitInfo = model.getExitSettingsInfo();
+        textGraphics.setForegroundColor(new TextColor.RGB(217, 167, 164));
+        textGraphics.setBackgroundColor(new TextColor.RGB(255, 225, 237));
+        textGraphics.putString(centerX - exitInfo.length() / 2, centerY - boxHeight / 2 - 2, exitInfo, SGR.BOLD);
+
+        // Enter Info
+        String enterInfo = model.getEnterSettingsInfo();
+        textGraphics.setForegroundColor(new TextColor.RGB(217, 167, 164));
+        textGraphics.setBackgroundColor(new TextColor.RGB(255, 225, 237));
+        textGraphics.putString(centerX - enterInfo.length() / 2, centerY + 9, enterInfo, SGR.BOLD);
+
     }
 
     private void drawBox(TextGraphics textGraphics, int startX, int startY, int width, int height){
